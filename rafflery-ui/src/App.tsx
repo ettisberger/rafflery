e14da26d5a26cafcf8157a3f9e6be208b57a9d3d
enpayne <nick.payne89@gmail.com>
2019-05-18 19:29:47 +0800

    set up ui

---
+++ rafflery-ui/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Rafflery</h1>
+      </div>
+      <div className="content">
+        <p>Hoi Andreas</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
