3979b3fce3b641f8e1153722658469d9fd04f209
Andy Ettisberger <andy@ettisberger.ch>
2019-05-22 19:53:31 +0200

    Header improvement

--- rafflery-ui/src/layout/header/Header.tsx
+++ rafflery-ui/src/layout/header/Header.tsx
@@ -5,13 +5,15 @@ import Navigation from './Navigation';
 const Header: React.FC = () => {
     return (
         <div className="header">
-            <div className="title">
-                <h1>RAFFLERY</h1>
-                <p>
-                    slots, prizes, chances
-                </p>
-            </div>
             <Navigation/>
+            <div className="content">
+                <div className="title">
+                    <h1>RAFFLERY</h1>
+                    <p>slots</p>
+                    <p>prizes</p>
+                    <p>chances</p>
+                </div>
+            </div>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
                 <polygon fill="white" points="0,100 100,40 100,100"/>
             </svg>
