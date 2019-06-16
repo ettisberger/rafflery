f69b58b298a24d4c03b694d0cd7a29527ff24140
Andy Ettisberger <andy@ettisberger.ch>
2019-06-16 13:04:32 +0200

    Add jwt authentication to route

--- rafflery-server/src/main/kotlin/ch/rafflery/app/App.kt
+++ rafflery-server/src/main/kotlin/ch/rafflery/app/App.kt
@@ -2,7 +2,11 @@ package ch.rafflery.app
 
 import ch.rafflery.controllers.raffleRoutes
 import ch.rafflery.infrastructure.CommandBus
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.application.call
+import io.ktor.application.install
 import io.ktor.http.ContentType
 import io.ktor.http.content.files
 import io.ktor.http.content.static
@@ -12,6 +16,11 @@ import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.auth.*
+import io.ktor.auth.jwt.JWTPrincipal
+import io.ktor.auth.jwt.jwt
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
 import java.io.File
 import javax.inject.Inject
 
@@ -20,7 +29,28 @@ const val STATIC_ROUTE = "./rafflery-ui/build/"
 class App @Inject constructor(private val commandBus: CommandBus) {
 
     fun start() {
+        val issuer = "https://jwt-provider-domain/"
+        val audience = "jwt-audience"
+        val realm = "ktor rafflery"
+
         val server = embeddedServer(Netty, port = 8080) {
+
+            install(DefaultHeaders)
+            install(CallLogging) // log every call
+            install(Authentication) {
+                val jwtVerifier = makeJwtVerifier(issuer, audience)
+                jwt {
+                    verifier(jwtVerifier)
+                    this.realm = realm
+                    validate { credential ->
+                        if (credential.payload.audience.contains(audience))
+                            JWTPrincipal(credential.payload)
+                        else
+                            null
+                    }
+                }
+            }
+
             routing {
                 raffleRoutes(commandBus)
                 get("/") {
@@ -29,6 +59,13 @@ class App @Inject constructor(private val commandBus: CommandBus) {
                 get("/api/hello") {
                     call.respondText("Hello World!", ContentType.Text.Plain)
                 }
+
+                authenticate {
+                    get("/api/secured") {
+                        call.respondText("This is highly secured by jwt tokens.")
+                    }
+                }
+
                 static {
                     files(STATIC_ROUTE)
                 }
@@ -37,5 +74,12 @@ class App @Inject constructor(private val commandBus: CommandBus) {
 
         server.start(wait = true)
     }
+
+    private val algorithm = Algorithm.HMAC256("secret")
+    private fun makeJwtVerifier(issuer: String, audience: String): JWTVerifier = JWT
+            .require(algorithm)
+            .withAudience(audience)
+            .withIssuer(issuer)
+            .build()
 }
 
