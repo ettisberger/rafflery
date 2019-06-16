96d3894541a2c64eeb082f226c953e26a775b896
Andy Ettisberger <andy@ettisberger.ch>
2019-06-16 14:22:25 +0200

    Add client id / issuer

--- rafflery-server/src/main/kotlin/ch/rafflery/app/App.kt
+++ rafflery-server/src/main/kotlin/ch/rafflery/app/App.kt
@@ -33,21 +33,22 @@ import java.security.interfaces.RSAPublicKey
 import java.util.concurrent.TimeUnit
 
 
-const val STATIC_ROUTE = "./rafflery-ui/build/"
+const val STATIC_ROUTE = "./rafflery-ui/build/" // TODO application.conf
 
 class App @Inject constructor(private val commandBus: CommandBus) {
 
     fun start() {
-        val issuer = "https://dev-gm0vrgh2.eu.auth0.com/" // domain
-        val audience = "71OoWiZhuBBYSLczfzhDXsO0DE37GS46" // client id
+        val issuer = "https://dev-gm0vrgh2.eu.auth0.com/" // // TODO application.conf DOMAIN
+        val audience = "71OoWiZhuBBYSLczfzhDXsO0DE37GS46" // // TODO application.conf CLIENT ID
         val jwkProvider = JwkProviderBuilder(issuer) // uses jwt keyset
                 .cached(10, 24, TimeUnit.HOURS)
                 .rateLimited(10, 1, TimeUnit.MINUTES)
                 .build()
+
         val server = embeddedServer(Netty, port = 8080) {
 
             install(DefaultHeaders)
-            install(CallLogging) // log every call
+            install(CallLogging) // log every rest call
             install(Authentication) {
                 jwt {
                     verifier(jwkProvider, issuer)
