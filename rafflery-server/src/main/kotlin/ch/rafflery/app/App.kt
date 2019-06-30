1d88e991cf185e171aced451d48806e31fa17ad6
enpayne <nick.payne89@gmail.com>
2019-06-30 18:13:04 +0800

    merge develop into master

--- rafflery-server/src/main/kotlin/ch/rafflery/app/App.kt
+++ rafflery-server/src/main/kotlin/ch/rafflery/app/App.kt
@@ -1,15 +1,22 @@
 package ch.rafflery.app
 
 import ch.rafflery.controllers.Controller
+import com.auth0.jwk.JwkProviderBuilder
 import io.ktor.application.Application
 import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.auth.jwt.JWTPrincipal
+import io.ktor.auth.jwt.jwt
+import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
 import io.ktor.gson.gson
 import io.ktor.http.content.files
 import io.ktor.http.content.static
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 const val STATIC_ROUTE = "./rafflery-ui/build/"
