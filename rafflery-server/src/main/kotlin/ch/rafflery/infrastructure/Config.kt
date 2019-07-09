33081e93f3bbc51404049efbd161db649915e72d
Andy Ettisberger <andy@ettisberger.ch>
2019-07-09 21:55:04 +0200

    Extend configuration loading with optional resource

--- rafflery-server/src/main/kotlin/ch/rafflery/infrastructure/Config.kt
+++ rafflery-server/src/main/kotlin/ch/rafflery/infrastructure/Config.kt
@@ -4,47 +4,69 @@ import com.natpryce.konfig.*
 import com.natpryce.konfig.ConfigurationProperties.Companion.systemProperties
 
 object Config {
+    // app config
+    private val app_port = "port" to intType
 
-  // app config
-  private val app_port = "port" to intType
+    // jwt config
+    private val jwt_clientId = "jwt.clientId" to stringType
+    private val jwt_secret = "jwt.secret" to stringType
+    private val jwt_issuer = "jwt.issuer" to stringType
 
-  // jwt config
-  private val jwt_clientId = "jwt.clientId" to stringType
-  private val jwt_secret = "jwt.secret" to stringType
-  private val jwt_issuer = "jwt.issuer" to stringType
+    // raffler config
+    private val raffler_apiUrl = "raffler.apiUrl" to stringType
+    private val raffler_apiKey = "raffler.apiKey" to stringType
 
-  private val config = systemProperties() overriding
-    EnvironmentVariables().also {
-      println("ENVIRONMENT PORT: ${it.getOrNull(Key("port", intType))}")
-      println("ENVIRONMENT JWT CLIENT ID: ${it.getOrNull(Key("JWT_CLIENTID", stringType))}")
-      println("ENVIRONMENT JWT ISSUER: ${it.getOrNull(Key("JWT_ISSUER", stringType))}")
-    }
-    // why does this give an error on heroku because he cant find the properties file even
-    // tho he finds every ENV var he needs?
-    //ConfigurationProperties.fromResource("local.properties")
+    private val config = systemProperties() overriding
+            EnvironmentVariables().also {
+                println("ENVIRONMENT PORT: ${it.getOrNull(Key("port", intType))}")
+                println(
+                    "ENVIRONMENT JWT CLIENT ID: ${it.getOrNull(
+                        Key(
+                            "JWT_CLIENTID",
+                            stringType
+                        )
+                    )}"
+                )
+                println("ENVIRONMENT JWT ISSUER: ${it.getOrNull(Key("JWT_ISSUER", stringType))}")
+            } overriding
+            ConfigurationProperties.fromOptionalResource("local.properties")
 
-  val jwtConfig
-    get() = JwtConfig(
-      config[jwt_clientId],
-      config[jwt_secret],
-      config[jwt_issuer]
-    ).also { println("Using JWT config: $it") }
+    val jwtConfig
+        get() = JwtConfig(
+            config[jwt_clientId],
+            config[jwt_secret],
+            config[jwt_issuer]
+        ).also { println("Using JWT config: $it") }
 
-  val appConfig
-    get() = AppConfig(
-        config[app_port]
-    )
+    val appConfig
+        get() = AppConfig(
+            config[app_port]
+        )
+
+    val rafflerConfig
+        get() = RafflerConfig(
+            config[raffler_apiKey],
+            config[raffler_apiUrl]
+        )
 }
 
 data class AppConfig(
     val port: Int
 )
 
-data class JwtConfig(
-  val clientId: String,
-  val secret: String,
-  val issuer: String
+data class RafflerConfig(
+    val apiKey: String,
+    val apiUrl: String
 )
 
-private infix fun <T> String.to(type: PropertyType<T>): Key<T> =
-  Key(this, type)
+data class JwtConfig(
+    val clientId: String,
+    val secret: String,
+    val issuer: String
+)
+
+private infix fun <T> String.to(type: PropertyType<T>): Key<T> = Key(this, type)
+
+private fun ConfigurationProperties.Companion.fromOptionalResource(resourceName: String) =
+    if (ClassLoader.getSystemResource(resourceName) != null) fromResource(resourceName)
+    else EmptyConfiguration
