d0830928f4dd7f3e1891effa07433d84fc1ee59e
Andy Ettisberger <andy@ettisberger.ch>
2019-07-19 19:42:11 +0200

    ktlint

--- rafflery-server/src/main/kotlin/ch/rafflery/infrastructure/Config.kt
+++ rafflery-server/src/main/kotlin/ch/rafflery/infrastructure/Config.kt
@@ -4,44 +4,44 @@ import com.natpryce.konfig.* // ktlint-disable no-wildcard-imports
 import com.natpryce.konfig.ConfigurationProperties.Companion.systemProperties
 
 object Config {
-  // app config
-  private val app_port = "port" to intType
-  private val environment = "environment" to stringType
+    // app config
+    private val app_port = "port" to intType
+    private val environment = "environment" to stringType
 
-  // jwt config
-  private val jwt_clientId = "jwt.clientId" to stringType
-  private val jwt_issuer = "jwt.issuer" to stringType
+    // jwt config
+    private val jwt_clientId = "jwt.clientId" to stringType
+    private val jwt_issuer = "jwt.issuer" to stringType
 
-  private val config = systemProperties() overriding
-    EnvironmentVariables() overriding
-    ConfigurationProperties.fromOptionalResource("local.properties") overriding
-    ConfigurationProperties.fromResource("default.properties")
+    private val config = systemProperties() overriding
+            EnvironmentVariables() overriding
+            ConfigurationProperties.fromOptionalResource("local.properties") overriding
+            ConfigurationProperties.fromResource("default.properties")
 
-  val jwtConfig
-    get() = JwtConfig(
-      config[jwt_clientId],
-      config[jwt_issuer]
-    )
+    val jwtConfig
+        get() = JwtConfig(
+            config[jwt_clientId],
+            config[jwt_issuer]
+        )
 
-  val appConfig
-    get() = AppConfig(
-      port = config[app_port],
-      environment = config[environment]
-    )
+    val appConfig
+        get() = AppConfig(
+            port = config[app_port],
+            environment = config[environment]
+        )
 }
 
 data class AppConfig(
-  val port: Int,
-  val environment: String
+    val port: Int,
+    val environment: String
 )
 
 data class JwtConfig(
-  val clientId: String,
-  val issuer: String
+    val clientId: String,
+    val issuer: String
 )
 
 private infix fun <T> String.to(type: PropertyType<T>): Key<T> = Key(this, type)
 
 private fun ConfigurationProperties.Companion.fromOptionalResource(resourceName: String) =
-  if (ClassLoader.getSystemResource(resourceName) != null) fromResource(resourceName)
-  else EmptyConfiguration
+    if (ClassLoader.getSystemResource(resourceName) != null) fromResource(resourceName)
+    else EmptyConfiguration
