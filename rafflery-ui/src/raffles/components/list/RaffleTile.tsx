2dd09dea8abe7187d922f822fdac8aa5910b32fa
enpayne <nick.payne89@gmail.com>
2019-07-20 14:52:31 +0800

    some styling

--- rafflery-ui/src/raffles/components/list/RaffleTile.tsx
+++ rafflery-ui/src/raffles/components/list/RaffleTile.tsx
@@ -2,21 +2,25 @@ import React from 'react';
 import './RaffleTile.css';
 import { Raffle } from '../../../types/Types';
 
-interface RaffleTileProps {
+export interface RaffleTileProps {
     raffle: Raffle;
     color: string;
 }
 
+
 const RaffleTile: React.FC<RaffleTileProps> = ({ raffle, color }) => {
+    const numberOfSlots = raffle.item.value / raffle.slotSize;
+
     return (
         <div className="raffleTile">
-            <div className="content" style={{backgroundColor:  color }}>
+            <div className="content" style={{backgroundColor: color}}>
                 <div className="title">
                     {raffle.name}
                 </div>
-                <div>SLOTS {raffle.slotSize}</div>
-                <div>SLOT PRIZE {raffle.item.value}</div>
-                <div>created by {raffle.createdBy}</div>
+                <div className="raffle-info">SLOT SIZE {raffle.slotSize.toLocaleString()}</div>
+                <div className="raffle-info">VALUE CHF {raffle.item.value.toLocaleString()}</div>
+                <div className="raffle-info">{raffle.purchasedTickets.length} / {numberOfSlots} sold</div>
+                <div className="raffle-info">created by {raffle.createdBy}</div>
             </div>
         </div>
     );
--- rafflery-ui/src/raffles/raffles.testData.ts
+++ rafflery-ui/src/raffles/raffles.testData.ts
@@ -1,10 +1,12 @@
 import { Raffle } from '../types/Types';
 
-export const someRaffles: Raffle[] = [{
+export const aRaffle: Raffle = {
   id: '1',
   name: 'my raffle',
   createdBy: 'nicholas',
   item: { name: 'powerball', value: 2000 },
   purchasedTickets: [],
   slotSize: 100
-}];
+};
+
+export const someRaffles: Raffle[] = [aRaffle];
