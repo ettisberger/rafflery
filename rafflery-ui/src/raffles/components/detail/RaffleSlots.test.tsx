b2e14d28bef975bb5a6fd79fc6dc86b951f666e3
Andy Ettisberger <andy@ettisberger.ch>
2019-07-25 21:50:02 +0200

    Add test for raffle slots component

--- rafflery-ui/src/raffles/components/detail/RaffleSlots.test.tsx
+++ rafflery-ui/src/raffles/components/detail/RaffleSlots.test.tsx
@@ -1,18 +1,30 @@
 import { cleanup, render } from '@testing-library/react';
-import { RaffleSlot, RaffleSlotProps } from './RaffleSlot';
 import * as React from 'react';
+import { RaffleSlots, RaffleSlotsProps } from './RaffleSlots';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('Raffle Slot', () => {
-  const defaultProps: RaffleSlotProps = {
-    slot: 1,
-    sold: true,
+  const defaultProps: RaffleSlotsProps = {
+    maxSlots: 10,
+    soldSlots: [5, 6],
   };
 
   afterEach(cleanup);
 
-  it('should display the slot number', () => {
-    const component = render(<RaffleSlot {...defaultProps} />);
+  it('should display slot elements', () => {
+    const component = render(<RaffleSlots {...defaultProps} />);
 
-    expect(component.getByText('1')).toBeDefined();
+    const slotElements = component.getAllByTestId('slot');
+
+    expect(slotElements.length).toBe(defaultProps.maxSlots);
+
+    slotElements.forEach((slot, i) => {
+      expect(slot).toHaveAttribute('unselectable');
+      if (defaultProps.soldSlots.includes(i)) {
+        expect(slot).toHaveClass('sold');
+      } else {
+        expect(slot).not.toHaveClass('sold');
+      }
+    });
   });
 });
