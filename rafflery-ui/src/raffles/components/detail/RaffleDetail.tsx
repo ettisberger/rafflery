d9cc0fe8114377fec8dd7b1b6f27ef347a16ad2d
Andy Ettisberger <andy@ettisberger.ch>
2019-08-05 19:19:10 +0200

    Add detail boxes

--- rafflery-ui/src/raffles/components/detail/RaffleDetail.tsx
+++ rafflery-ui/src/raffles/components/detail/RaffleDetail.tsx
@@ -15,11 +15,33 @@ export const RaffleDetail: React.FC<RaffleDetailProps> = ({
   return (
     <div className="raffle-detail" data-testid="raffle-detail">
       <h2>Purchase a slot for {raffle.name}</h2>
+      <div className="raffle-detail-boxes">
+        <div className="raffle-detail-box">
+          <div className="raffle-detail-box__title">{raffle.slotSize}</div>
+          <div className="raffle-detail-box__description">SLOTS</div>
+        </div>
+        <div className="raffle-detail-box">
+          <div className="raffle-detail-box__title">{raffle.item.value}</div>
+          <div className="raffle-detail-box__description">PRICE</div>
+        </div>
+        <div className="raffle-detail-box">
+          <div className="raffle-detail-box__title">
+            {raffle.slotSize - raffle.purchasedTickets.length}
+          </div>
+          <div className="raffle-detail-box__description">SLOTS LEFT</div>
+        </div>
+        <div className="raffle-detail-box">
+          <div className="raffle-detail-box__title">
+            {raffle.purchasedTickets.length}
+          </div>
+          <div className="raffle-detail-box__description">SLOTS SOLD</div>
+        </div>
+      </div>
       <RaffleSlots
         maxSlots={raffle.slotSize}
         soldSlots={raffle.purchasedTickets.map(owner => owner.slot)}
       />
-      <div className="raffle-detail__actions">
+      <div className="raffle-detail-actions">
         <button>Purchase</button>
         <button onClick={onCancel}>Cancel</button>
       </div>
